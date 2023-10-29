6d32ff58f424b208115c83257c73eec5030befb3
Jacob Bogdany <jacob.bogdany@uconn.edu>
2023-10-29 13:41:49 -0400

    Update to Dashboard

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,9 +1,21 @@
-import { Text, Div, Button, Header, Icon } from "react-native-magnus";
+import { Text, Div, Button, Header, Icon, Image} from "react-native-magnus";
 
 export default function HomePage() {
+    
   return (
-    <Div w={"100%"}>
-      <Header
+    <Div 
+      style={{
+        backgroundColor: "#fff5f7",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Text
+        style={{
+        backgroundColor: "#fff5f7",
+        height: "10%",
+        width: "100%",
+        }}
         p="lg"
         alignment="left"
         prefix={
@@ -16,11 +28,42 @@ export default function HomePage() {
             <Icon name="more-vertical" fontFamily="Feather" />
           </Button>
         }
-      >
-        Dashboard
-      </Header>
+      ></Text>
+        
+      
       <Div m={10}>
-        <Text>Dashboard</Text>
+        <Text>Activity</Text>
+        <Text>Coming Soon</Text>
+        <Text>Course ID:</Text>
+        
+        <Button
+          onPress={() => {
+            // handle sign in with Google logic
+          }}
+          bg="white"
+          alignSelf="center"
+          fontSize={20}
+          fontWeight="bold"
+          w={300}
+          m={10}
+          mb={150}
+          ><Text color="">Join Course</Text>
+          </Button>
+          
+          <Button
+          onPress={() => {
+            // handle sign in with Google logic
+          }}
+          bg="white"
+          alignSelf="center"
+          fontSize={20}
+          fontWeight="bold"
+          w={300}
+          m={10}
+          mb={150}
+          >
+            
+            <Text color="">Join our Discord</Text><Image source={require("../assets/DiscordLogo.png")} h={30} w={30} /></Button>
       </Div>
     </Div>
   );
